9127fcaad6bf3bfe89eb5a3519209944f57ada67
UltralyticsAssistant <web@ultralytics.com>
2026-04-08 15:34:16 +0000

    Auto-format by https://ultralytics.com/actions

--- my_predict_R.py
+++ my_predict_R.py
@@ -1,10 +1,6 @@
-import cv2
-import matplotlib.pyplot as plt
-import numpy as np
 from ultralytics import YOLO
-import cvzone
 
 # Use 0 for built-in camera, 1 or 2 for external
 model = YOLO("/Users/hayhay/Documents/ultralytics-main/runs/detect/train11/weights/best.pt")
 results = model.predict(source=0, show=True)
-print (results)
+print(results)
